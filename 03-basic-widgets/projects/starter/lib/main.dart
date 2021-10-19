62fa7334476f608b73ef256b91f75513fe1bf174
Nichole De La Cruz <nichole@sudoprime.com>
2021-10-19 09:40:25 -0700

    Initial project setup and style the app

--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -1,27 +1,46 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 void main() {
   // 1
+  // Everything in Flutter starts with a widget.
+  //`runApp()` takes in the root widget Fooderlich.
   runApp(const Fooderlich());
 }
 
 class Fooderlich extends StatelessWidget {
   // 2
+  // constructor({ key }) {
+  //   super({ key })
+  // }
   const Fooderlich({Key? key}) : super(key: key);
+  // Every stateless widget must override the build method.
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
+    // Define the variable that holds the theme
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
     // 3
+    // Compose a `MaterialApp` widget to give it a Material Design system look and feel
     return MaterialApp(
-      // TODO: Add theme
+      theme: theme,
       title: 'Fooderlich',
       // 4
+      // `Scaffold` widget defines the layout and structure of the app
       home: Scaffold(
         // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
-        // TODO: Style the body text
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: theme.textTheme.headline6,
+          ),
+        ),
+        body: Center(
+          child: Text(
+            'Let\'s get cooking 👩‍🍳',
+            style: theme.textTheme.headline1,
+          ),
+        ),
       ),
     );
   }
