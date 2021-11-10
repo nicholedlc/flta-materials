4833ad58212f192c2050da0d66484e78553deba5
Nichole De La Cruz <nichole@sudoprime.com>
2021-11-10 14:44:01 -0800

    Create the empty grocery screen

---
+++ 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:flutter/material.dart';
+
+class EmptyGroceryScreen extends StatelessWidget {
+  const EmptyGroceryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // `Flexible` gives a child the ability to fill the available space in the main axis
+            Flexible(
+              // Although `aspectRatio` is a double, the Flutter documentation recommends writing it as `width/height` instead of the calculated result.
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+              ),
+            ),
+            const Text(
+              'No Groceries',
+              style: TextStyle(fontSize: 21.0),
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Shopping for ingredients?\n'
+              'Tap the + button to write them down!',
+              textAlign: TextAlign.center,
+            ),
+            MaterialButton(
+              textColor: Colors.white,
+              child: const Text('Browse Recipes'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              color: Colors.green,
+              onPressed: () {
+                // TODO 8: Go to Recipes Tab
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  // TODO: Add buildGroceryScreen
+}
